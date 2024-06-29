e0def547f5c7b1c92011608dc0055e0f138642b4
apotatodev <inthenextfuture@gmail.com>
2024-06-29 04:48:06 -0400

    still fixing client images

--- frontend/utils.js
+++ frontend/utils.js
@@ -1,5 +1,19 @@
+/**
+ * @returns {Promise<PromiseSettledResult<HTMLImageElement>[]>}
+ */
 const loadImages = async function(){
-
+    const response = await fetch("https://raw.githubusercontent.com/thingthree3/iogamedemo/main/backend/Tiled-levelEditor/level-data/images.json");
+    if(!response.ok)
+        throw new Error(response.statusText);
+    const json = await response.json();
+    return Promise.allSettled(json.map(imageData => {
+        const img = new Image();
+        // we might need to add a retry mechanism here
+        img.onerror = function(){throw new Error(`Failed to load image ${imageData.src}`);};
+        const loadedImage = new Promise(res => img.onload = res(img));
+        img.src = imageData.src;
+        return loadedImage;
+    }));
 }
 
 export { loadImages };
